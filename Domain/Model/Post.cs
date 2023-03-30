4e28115a6c19b59001fbb71dd91f89f5fe99a546
GabrielaDicusar <gabrieladicusar@gmail.com>
2023-03-30 12:28:01 +0200

    File Data storage stage

---
+++ Domain/Model/Post.cs
@@ -0,0 +1,13 @@
+﻿namespace Shared;
+
+public class Post
+{
+    public string post;
+    public User user;
+
+    public Post(string post, User user)
+    {
+        this.post = post;
+        this.user = user;
+    }
+}
---
+++ FileData/DataContainer.cs
@@ -0,0 +1,10 @@
+﻿using Shared;
+
+namespace FileData;
+
+//This class holds information before storing it in the JSON file
+public class DataContainer
+{
+    public ICollection<User> Users { get; set; }
+    public ICollection<Post> Posts { get; set; }
+}
