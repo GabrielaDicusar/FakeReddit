4cc0db0d8765adabfc43c302bb2755b05e685e83
GabrielaDicusar <gabrieladicusar@gmail.com>
2023-04-09 13:52:42 +0200

    Modified Create user - with password

--- Application/Logic/UserLogic.cs
+++ Application/Logic/UserLogic.cs
@@ -24,7 +24,8 @@ public class UserLogic: IUserLogic
         ValidateData(dto);
         User toCreate = new User
         {
-            UserName = dto.UserName
+            UserName = dto.UserName,
+            Password = dto.Password
         };
     
         User created = await userDao.CreateAsync(toCreate);
@@ -35,11 +36,15 @@ public class UserLogic: IUserLogic
     private static void ValidateData(UserCreationDto userToCreate)
     {
         string userName = userToCreate.UserName;
+        string password = userToCreate.Password;
+
 
         if (userName.Length < 3)
             throw new Exception("Username must be at least 3 characters!");
 
         if (userName.Length > 15)
             throw new Exception("Username must be less than 16 characters!");
+        if (password.Length < 3)
+            throw new Exception("Password must be at least 3 characters!");
     }
 }
--- Domain/DTOs/UserCreationDto.cs
+++ Domain/DTOs/UserCreationDto.cs
@@ -2,10 +2,12 @@
 //This is just some kind of blue print for the actual user. The UserController will use the UserLogic to create the 3D version of it
 public class UserCreationDto
 {
-    public string UserName{ get; set; }
+    public string UserName{ get; init; }
+    public string Password{ get; init; }
 
-    public UserCreationDto(string userName)
+    public UserCreationDto(string userName, string password)
     {
         UserName = userName;
+        Password = password;
     }
 }
